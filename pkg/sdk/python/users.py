d1292c13ef0458e62a4110ddf780671cb7c770f6
Filip Bugarski <filipbugarski@gmail.com>
2021-01-15 14:14:56 +0100

    Update users SDK function
    Added functions that implement users Mainflux
    service API calls.
    
    Signed-off-by: Filip Bugarski <filipbugarski@gmail.com>

--- pkg/sdk/python/users.py
+++ pkg/sdk/python/users.py
@@ -1,14 +1,27 @@
 import requests
+import json
 
-def CreateUser(email,password):
-  user = {
-    "email":email,
-    "password":password
-  }
+def CreateUser(user):
+  ''' CreateUser() creates user entity in the database '''
   resp = requests.post("http://localhost/users", json=user)
   return resp.headers.get("location")
 
-# id = CreateUser("flp2@gmail.com","12345678")
-# if id is not None:  
-#   a = id.split('/')
-#   print(a[2])
+def User(token):
+  resp = requests.get("http://localhost/users", headers={"Authorization": token})
+  return resp.content
+
+def CreateToken(user):
+  resp = requests.post("http://localhost/tokens",json=user)
+  return resp.json()["token"]
+
+def UpdateUser(user, token):
+  resp = requests.put("http://localhost/users", headers={"Authorization": token}, data=json.dumps(user))
+  return resp.status_code
+
+def UpdatePassword(old_password, new_password, token):
+  payload = {
+    "password":old_password,
+    "old_password":new_password
+  }
+  resp = requests.patch("http://localhost/password", headers={"Authorization": token}, data=json.dumps(payload))
+  return resp
