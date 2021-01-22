c72fd18059052576c0341e75a08cb393a28f9d8f
Filip Bugarski <filipbugarski@gmail.com>
2021-01-22 13:37:20 +0100

    Add things and channels
    This commit adds SDK functions for
    the things and channels microservices.
    
    Signed-off-by: Filip Bugarski <filipbugarski@gmail.com>

---
+++ pkg/sdk/python/channels.py
@@ -0,0 +1,32 @@
+import request
+import json
+
+def CreateChannel(channel, token):
+    resp = requests.post("http://localhost/channels", json=channel, headers={"Authorization": token})
+    return resp.headers.get("location")
+
+def CreateChannels(channels, token):
+    resp = requests.post("http://localhost/channels", json=channels, headers={"Authorization": token})
+    return resp
+
+def Channels(params, token):
+    url = "http://localhost/channels"+'?'+'offset='+params['offset']+'&'+'limit='+params['limit']+'&'+'connected='+params['connected']
+    resp = requests.get(url, headers={"Authorization": token})
+    return resp.json()
+
+def ChannelsByThing(thingID, params, token):
+    url = "http://localhost/things"+thingID+'/channels'+'?'+'offset='+params['offset']+'&'+'limit='+params['limit']+'&'+'connected='+params['connected']
+    resp = requests.post(url, json=channels, headers={"Authorization": token})
+    return resp
+
+def Channel(id, token):
+    resp = requests.get("http://localhost/channels/"+id, headers={"Authorization": token})
+    return resp
+
+def UpdateChannel(channel, token):
+    resp= requests.put("http://localhost/channels/"+channel["id"], headers={"Authorization": token}, json = channel) 
+    return resp
+
+def DeleteChannel(id, token):
+    resp = requests.delete("http://localhost/channels/"+id, headers={"Authorization": token})
+    return resp
---
+++ pkg/sdk/python/things.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+def CreateThing(thing, token):
+    resp = requests.post("http://localhost/things", json=thing, headers={"Authorization": token})
+    return resp.headers.get("location")
+
+def CreateThings(things, token):
+    resp = requests.post("http://localhost/things", json=things, headers={"Authorization": token})
+    return resp
+
+def Things(params, token):
+    url = "http://localhost/things"+'?'+'offset='+params['offset']+'&'+'limit='+params['limit']+'&'+'name='+params['name']
+    resp = requests.get(url, headers={"Authorization": token})
+    return resp.json()
+
+def ThingsByChannel(channelID, params, token):
+    url = "http://localhost/channels"+channelID+'/things'+'?'+'offset='+params['offset']+'&'+'limit='+params['limit']+'&'+'connected='+params['connected']
+    resp = requests.post(url, json=channels, headers={"Authorization": token})
+    return resp
+
+def Thing(id, token):
+    resp = requests.get("http://localhost/things/"+id, headers={"Authorization": token})
+    return resp
+
+def UpdateThing(thing, token):
+    resp= requests.put("http://localhost/things/"+thing["id"], headers={"Authorization": token}, json = thing) 
+    return resp
+
+def DeleteThing(id, token):
+    resp = requests.delete("http://localhost/things/"+id, headers={"Authorization": token})
+    return resp
