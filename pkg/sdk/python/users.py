2961011a7c4cbb32cfb357e978345d77b22a2179
Filip Bugarski <filipbugarski@gmail.com>
2021-02-10 13:15:26 +0100

    ADDED status, error and msg
    Signed-off-by: Filip Bugarski <filipbugarski@gmail.com>

--- pkg/sdk/python/users.py
+++ pkg/sdk/python/users.py
@@ -1,4 +1,5 @@
 import requests
+import response
 import json
 
 
@@ -8,28 +9,96 @@ class Users:
 
     def create(self, user):
         '''Creates user entity in the database'''
-        resp = requests.post(self.url + "/users", json=user)
-        return resp.headers.get("location").split('/')[2]
-
-    def get_all(self, token):
-        '''Gets all users from database'''
-        resp = requests.get(self.url + "/users", headers={"Authorization": token})
-        return resp.content
-
-    def get(self, id, token):
-        '''Gets a user entity for a logged-in user'''
-        resp = requests.get(self.url + "/users/" + id, headers={"Authorization": token})
-        return resp.content
+        mf_resp = response.Response()
+        http_resp = requests.post(self.url + "/users", json=user)
+        if http_resp.status_code != 201:
+            mf_resp.error.status = 1
+            c = http_resp.status_code
+            if c == 400:
+                mf_resp.error.message = "Failed due to malformed JSON"
+            if c == 409:
+                mf_resp.error.message = "Failed due to using an existing email address"
+            if c == 415:
+                mf_resp.error.message = "Missing or invalid content type"
+            if c == 500:
+                mf_resp.error.message = "Unexpected server-side error occurred"
+        else:
+            location = http_resp.headers.get("location")
+            mf_resp.value = location.split('/')[2]
+        return mf_resp
 
     def login(self, user):
         '''Creates a user token'''
-        resp = requests.post(self.url + "/tokens", json=user)
-        return resp.json()["token"]
+        mf_resp = response.Response()
+        http_resp = requests.post(self.url + "/tokens", json=user)
+        if http_resp.status_code != 201:
+            mf_resp.error.status = 1
+            c = http_resp.status_code
+            if c == 400:
+                mf_resp.error.message = "Failed due to malformed JSON"
+            if c == 409:
+                mf_resp.error.message = "Failed due to using an existing email address"
+            if c == 415:
+                mf_resp.error.message = "Missing or invalid content type"
+            if c == 500:
+                mf_resp.error.message = "Unexpected server-side error occurred"
+        else:
+            t = http_resp.json()["token"]
+            mf_resp.value = t
+        return mf_resp
+
+    def get(self, id, token):
+        '''Gets a user entity for a logged-in user'''
+        mf_resp = response.Response()
+        http_resp = requests.get(self.url + "/users/" + id, headers={"Authorization": token})
+        if http_resp.status_code != 200:
+            mf_resp.error.status = 1
+            c = http_resp.status_code
+            if c == 400:
+                mf_resp.error.message = "Failed due to malformed query parameters"
+            if c == 403:
+                mf_resp.error.message = "Missing or invalid access token provided"
+            if c == 500:
+                mf_resp.error.message = "Unexpected server-side error occurred"
+        else:
+            mf_resp.value = http_resp.content.decode("utf-8")
+        return mf_resp
+
+    def get_all(self, token):
+        '''Gets all users from database'''
+        http_resp = requests.get(self.url + "/users", headers={"Authorization": token})
+        mf_resp = response.Response()
+        if http_resp.status_code != 200:
+            mf_resp.error.status = 1
+            c = http_resp.status_code
+            if c == 400:
+                mf_resp.error.message = "Failed due to malformed query parameters"
+            if c == 403:
+                mf_resp.error.message = "Missing or invalid access token provided"
+            if c == 500:
+                mf_resp.error.message = "Unexpected server-side error occurred"
+        else:
+            mf_resp.value = http_resp.content.decode("utf-8")
+        return mf_resp
 
     def update(self, user, token):
         '''Updates user entity'''
-        resp = requests.put(self.url + "/users", headers={"Authorization": token}, data=json.dumps(user))
-        return resp.status_code
+        http_resp = requests.put(self.url + "/users", headers={"Authorization": token}, data=json.dumps(user))
+        mf_resp = response.Response()
+        if http_resp.status_code != 200:
+            mf_resp.error.status = 1
+            c = http_resp.status_code
+            if c == 400:
+                mf_resp.error.message = "Failed due to malformed JSON"
+            if c == 403:
+                mf_resp.error.message = "Missing or invalid access token provided"
+            if c == 404:
+                mf_resp.error.message = "Failed due to non existing user"
+            if c == 500:
+                mf_resp.error.message = "Unexpected server-side error occurred"
+        else:
+            mf_resp.value = http_resp.status_code
+        return mf_resp
 
     def update_password(self, old_password, new_password, token):
         '''Changes user password'''
@@ -37,5 +106,17 @@ class Users:
           "old_password": old_password,
           "new_password": new_password
         }
-        resp = requests.patch(self.url + "/password", headers={"Authorization": token}, data=json.dumps(payload))
-        return resp
+        http_resp = requests.patch(self.url + "/password", headers={"Authorization": token}, data=json.dumps(payload))
+        mf_resp = response.Response()
+        if http_resp.status_code != 201:
+            mf_resp.error.status = 1
+            c = http_resp.status_code
+            if c == 400:
+                mf_resp.error.message = "Failed due to malformed JSON"
+            if c == 415:
+                mf_resp.error.message = "Missing or invalid content type"
+            if c == 500:
+                mf_resp.error.message = "Unexpected server-side error occurred"
+        else:
+            mf_resp.value = http_resp.status_code
+        return mf_resp
