b382fc7bbde245b4a79b305ee42a104bab6b1a6f
Filip Bugarski <filipbugarski@gmail.com>
2021-04-20 15:11:22 +0200

    Added tests and pkg
    Signed-off-by: Filip Bugarski <filipbugarski@gmail.com>

---
+++ pkg/sdk/python/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-fbugarski",
+    version="0.0.1",
+    author="Filip Bugarski",
+    author_email="filipbugarski@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mainflux/mainflux/tree/master/pkg/sdk/python",
+    project_urls={
+        "Bug Tracker": "https://github.com/mainflux/mainflux/tree/master/pkg/sdk/python",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.8.5",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
